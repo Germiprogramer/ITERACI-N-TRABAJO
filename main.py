e9cc3e667c6dc39c9e56c28991ef8c84d85e6d50
Germán Llorente <91720991+Germiprogramer@users.noreply.github.com>
2022-02-22 14:40:02 +0000

    PAUSA EJERCICIO 8

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #EJERCICIO 8
 
-entrada = input("Introduce una cadena de texto separada por :")
+entrada = input("Introduce una cadena de texto separada por :" )
 
 n = len(entrada)
-
-print(entrada[2])
 
 SEPARADOR = ":"
 
 for posicioncaracter in range(n):
   if entrada[posicioncaracter] == ":":
+    print(entrada[0:(posicioncaracter-1)])
     print(entrada[(posicioncaracter+1):n])
+
+lista = list(entrada)
+print(lista)
